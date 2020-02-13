b89c893760af34e79a32c47d09dcde758ff3c984
Artem <a.oktisyuk@gmail.com>
2020-02-13 15:59:22 +0200

    thrid section

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,52 @@
 // https://github.com/alvarotrigo/fullpage.js
-// new fullpage('#container');
+new fullpage('#container');
 
 // https://github.com/Mobius1/Pageable
-new Pageable('#container', {
-  animation: 700,
-  events: {
-    wheel: true,
-    mouse: true,
-    touch: true,
-    keydown: true,
-  },
+// new Pageable('#container', {
+//   animation: 700,
+//   events: {
+//     wheel: true,
+//     mouse: false,
+//     touch: true,
+//     keydown: true,
+//   },
+// });
+
+
+// Toggle active class
+function toggleClass(trigger, toggleClassName) {
+  const items = document.querySelectorAll(trigger);
+
+  if (items && items.length) {
+    for (let link of items) {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const tClass = this.dataset.toggleClass;
+        const wrapIdentify = this.dataset.toggleWrap;
+        let wrapToggleClass = 'active';
+
+        if (toggleClassName && toggleClassName.length) {
+          wrapToggleClass = toggleClassName;
+        } else if (tClass && tClass.length) {
+          wrapToggleClass = tClass;
+        }
+
+        this.classList.toggle(wrapToggleClass);
+
+        if (wrapIdentify) {
+          const wrap = document.querySelector(wrapIdentify);
+
+          if (wrap) {
+            wrap.classList.toggle(wrapToggleClass);
+          }
+        }
+      });
+    }
+  }
+}
+
+// Document ready function
+document.addEventListener("DOMContentLoaded", function() {
+  toggleClass('[data-toggle-wrap]');
+  toggleClass('[data-toggle-class]');
 });
